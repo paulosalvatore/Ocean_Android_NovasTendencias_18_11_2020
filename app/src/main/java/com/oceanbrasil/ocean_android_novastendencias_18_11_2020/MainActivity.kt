3c85f4fd7efd69c661e07898ae4bc704c68e7d51
Paulo Salvatore <paulo@salvatore.tech>
2020-11-18 14:55:15 -0300

    Finalização do binding do objeto game

--- app/src/main/java/com/oceanbrasil/ocean_android_novastendencias_18_11_2020/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_novastendencias_18_11_2020/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.oceanbrasil.ocean_android_novastendencias_18_11_2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.oceanbrasil.ocean_android_novastendencias_18_11_2020.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        // Tipo de classe de Binding gerada:
+        // ActivityMainBinding
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+
+        val donkeyKong = GameKt("Donkey Kong", 1981)
+
+        binding.game = donkeyKong
+
+//        tvName.text = donkeyKong.name
     }
 }
