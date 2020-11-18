22d11af1cf217a1e03789b5f80d647a851bc0701
Paulo Salvatore <paulo@salvatore.tech>
2020-11-18 15:58:32 -0300

    Bindable property

--- app/src/main/java/com/oceanbrasil/ocean_android_novastendencias_18_11_2020/model/domain/GameKt.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_novastendencias_18_11_2020/model/domain/GameKt.kt
@@ -1,7 +1,20 @@
 package com.oceanbrasil.ocean_android_novastendencias_18_11_2020.model.domain
 
-data class GameKt(
-    val name: String,
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
+
+class GameKt(
+    name: String,
     val launchYear: Int,
     val imageUrl: String = "URL_IMAGEM_DEFAULT"
-)
+) : BaseObservable() {
+    @get:Bindable
+    var name = name
+        set(value) {
+            if (field != value) {
+                field = value
+                notifyPropertyChanged(BR.name)
+            }
+        }
+}
